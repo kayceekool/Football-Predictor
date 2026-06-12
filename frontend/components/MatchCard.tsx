281ceba055f5e2bad2d0f524940b48c2ecc07fe8
kayceekool <115635448+kayceekool@users.noreply.github.com>
2026-06-12 15:24:45 +0100

    Create frontend/components/MatchCard.tsx

---
+++ frontend/components/MatchCard.tsx
@@ -0,0 +1,46 @@
+export default function MatchCard(
+{
+ prediction
+}
+:any
+){
+
+ return (
+
+  <div className="border rounded p-4">
+
+   <h2>{prediction.match}</h2>
+
+   <p>
+    Winner:
+    {prediction.winner}
+   </p>
+
+   <p>
+    O2.5:
+    {prediction.over25
+     ? "YES"
+     : "NO"}
+   </p>
+
+   <p>
+    BTTS:
+    {prediction.btts
+     ? "YES"
+     : "NO"}
+   </p>
+
+   <p>
+    Score:
+    {prediction.score}
+   </p>
+
+   <p>
+    Confidence:
+    {prediction.confidence}%
+   </p>
+
+  </div>
+
+ );
+}
