aeee0de057302608a5c2b1570c621688eaa01116
kayceekool <115635448+kayceekool@users.noreply.github.com>
2026-06-14 00:02:07 +0100

    Create frontend/app/page.tsx

---
+++ frontend/app/page.tsx
@@ -0,0 +1,69 @@
+import MatchCard
+
+from "@/components/MatchCard";
+
+
+
+import { getPredictions }
+
+from "@/lib/api";
+
+
+
+export default async function Home() {
+
+
+
+ const predictions =
+
+ await getPredictions();
+
+
+
+ return (
+
+
+
+  <main>
+
+
+
+   <h1>
+
+    Football AI Predictions
+
+   </h1>
+
+
+
+   {
+
+    predictions.map(
+
+      (prediction:any)=>(
+
+        <MatchCard
+
+          key={prediction.match}
+
+          prediction={prediction}
+
+        />
+
+      )
+
+    )
+
+   }
+
+
+
+  </main>
+
+
+
+ );
+
+
+
+}
