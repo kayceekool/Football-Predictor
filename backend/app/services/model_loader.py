dec4988f9a17522bc84c7622542521f268a90bc5
kayceekool <115635448+kayceekool@users.noreply.github.com>
2026-06-23 18:48:57 +0100

    Create backend/app/services/model_loader.py

---
+++ backend/app/services/model_loader.py
@@ -0,0 +1,17 @@
+import joblib
+
+winner_model = joblib.load(
+    "winner_model.pkl"
+)
+
+over15_model = joblib.load(
+    "over15_model.pkl"
+)
+
+over25_model = joblib.load(
+    "over25_model.pkl"
+)
+
+btts_model = joblib.load(
+    "btts_model.pkl"
+)
