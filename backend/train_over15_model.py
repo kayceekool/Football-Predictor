305c55eeaa5086b51741273743f84f7467e1227e
kayceekool <115635448+kayceekool@users.noreply.github.com>
2026-06-24 19:11:30 +0100

    Update train_over15_model.py

--- backend/train_over15_model.py
+++ backend/train_over15_model.py
@@ -1,24 +1,22 @@
 import pandas as pd
 import joblib
 
-from lightgbm import LGBMClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+
+from lightgbm import LGBMClassifier
 
 from feature_engineering import create_features
 
 df = pd.read_csv("data/matches.csv")
 
-df = create_features(df)
-
 df["over15"] = (
-    df["total_goals"] > 1
+    df["home_goals"] +
+    df["away_goals"]
+    > 1
 ).astype(int)
 
-X = df[[
-    "home_goals",
-    "away_goals"
-]]
+X = create_features(df)
 
 y = df["over15"]
 
@@ -31,18 +29,22 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 model = LGBMClassifier()
 
-model.fit(X_train, y_train)
+model.fit(
+    X_train,
+    y_train
+)
 
 preds = model.predict(X_test)
 
 print(
     "Over1.5 Accuracy:",
-    accuracy_score(y_test, preds)
+    accuracy_score(
+        y_test,
+        preds
+    )
 )
 
 joblib.dump(
     model,
     "over15_model.pkl"
 )
-
-print("over15_model.pkl saved")
