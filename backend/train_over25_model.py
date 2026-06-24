103c6eca058d58503a84196bed804868f0d7219d
kayceekool <115635448+kayceekool@users.noreply.github.com>
2026-06-24 19:12:09 +0100

    Update train_over25_model.py

--- backend/train_over25_model.py
+++ backend/train_over25_model.py
@@ -1,24 +1,22 @@
 import pandas as pd
 import joblib
 
-from lightgbm import LGBMClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+
+from lightgbm import LGBMClassifier
 
 from feature_engineering import create_features
 
 df = pd.read_csv("data/matches.csv")
 
-df = create_features(df)
-
 df["over25"] = (
-    df["total_goals"] > 2
+    df["home_goals"] +
+    df["away_goals"]
+    > 2
 ).astype(int)
 
-X = df[[
-    "home_goals",
-    "away_goals"
-]]
+X = create_features(df)
 
 y = df["over25"]
 
@@ -31,18 +29,22 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 model = LGBMClassifier()
 
-model.fit(X_train, y_train)
+model.fit(
+    X_train,
+    y_train
+)
 
 preds = model.predict(X_test)
 
 print(
     "Over2.5 Accuracy:",
-    accuracy_score(y_test, preds)
+    accuracy_score(
+        y_test,
+        preds
+    )
 )
 
 joblib.dump(
     model,
     "over25_model.pkl"
 )
-
-print("over25_model.pkl saved")
