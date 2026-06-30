d9f1b0dc36b8c870f95963ea17c7ada2c3409139
kayceekool <115635448+kayceekool@users.noreply.github.com>
2026-06-30 23:05:06 +0100

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,11 +5,17 @@ from app.init_db import init_db
 from app.api.predictions import router as prediction_router
 from app.api.matches import router as match_router
 from app.api.health import router as health_router
+from app.api.live_predictions import (
+    router as live_prediction_router
+)
+
 try:
     from app.api.fixtures import router as fixture_router
     print("Fixtures router loaded successfully")
 except Exception as e:
+    fixture_router = None
     print("Fixtures router failed:", e)
+
 app = FastAPI(
     title="Football Predictor API"
 )
@@ -34,10 +40,17 @@ app.include_router(
     tags=["health"]
 )
 
+if fixture_router:
+    app.include_router(
+        fixture_router,
+        prefix="/fixtures",
+        tags=["fixtures"]
+    )
+
 app.include_router(
-    fixture_router,
-    prefix="/fixtures",
-    tags=["fixtures"]
+    live_prediction_router,
+    prefix="/live",
+    tags=["Live Predictions"]
 )
 
 @app.get("/")
